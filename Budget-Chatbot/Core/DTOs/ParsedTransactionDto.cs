2b2bc63ad3d750f36aee4f7c5bb3b120a1b802e9
kawalerski-ja <kawalerski@student.agh.edu.pl>
2026-06-01 09:17:17 +0200

    DTOs setup

---
+++ Budget-Chatbot/Core/DTOs/ParsedTransactionDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BudgetChatbot.Core.DTOs;
+
+public class ParsedTransactionDto
+{
+    
+
+    [JsonPropertyName("amount")]
+    public decimal Amount { get; set; }
+
+    [JsonPropertyName("categoryId")]
+    public int CategoryId { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; } = string.Empty;
+}
